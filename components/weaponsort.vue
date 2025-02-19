8c326749f34440aa02c0647bf6b24c502c87647a
quickoabdul <quickoabdul@gmail.com>
2025-02-19 18:47:19 +0100

    update: WeaponSort animation

--- components/weaponsort.vue
+++ components/weaponsort.vue
@@ -2,23 +2,27 @@
   <div class="flex space-x-4 p-4">
     <div v-for="(weapon, index) in weapons" :key="index" class="relative">
       <div
-        class="bg-gray-100 border-gray-400 border-2 text-black px-6 py-2 rounded-lg cursor-pointer hover:bg-gray-600 hover:text-white"
-        @click="toggleDropdown(index)"
-      >
+        class="bg-gray-100 border-gray-400 border-2 text-black px-6 py-2 rounded-lg cursor-pointer 
+        hover:bg-gray-600 hover:text-white"
+        @click="toggleDropdown(index)">
         {{ weapon.name }}
       </div>
-
+      <transition
+        enter-active-class="transition duration-500 ease-in-out transform opacity-0 translate-y-4"
+        enter-to-class="opacity-100 translate-y-0"
+        leave-active-class="transition duration-300 ease-in transform opacity-100 translate-y-0"
+        leave-to-class="opacity-0 translate-y-4">
       <div
         v-if="dropdownIndex === index"
-        class="absolute top-full left-0 mt-2 bg-gray-700 text-white rounded-lg shadow-lg w-40 z-50"
-      >
+        class="absolute top-full left-0 mt-2 bg-gray-700 text-white rounded-lg shadow-lg w-40 z-50">
         <ul>
-          <li v-for="(option, i) in weapon.options" :key="i" class="px-4 py-2 hover:bg-gray-600 cursor-pointer">
+          <li v-for="(option, i) in weapon.options" :key="i" class="px-4 py-2 rounded-lg hover:bg-gray-600 cursor-pointer">
             {{ option }}
             <img v-if="weapon.images[i]" :src="weapon.images[i]" alt="Weapon Image" class=" w-full h-10" />
           </li>
         </ul>
       </div>
+      </transition>
     </div>
   </div>
 </template>
